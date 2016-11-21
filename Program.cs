71d7884b0d3b80f0b5a372fec34bc5d4b294152c
Tim Turner <timdturner@gmail.com>
2016-11-21 16:27:29 -0500

    Initial Check-in

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Discore;
+using Microsoft.Extensions.Configuration;
+
+namespace ShadowfactsDiscord
+{
+    public class Shadowfacts
+    {
+        private const string _token = "";
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                            .AddJsonFile("config.json");
+
+            var configuration = builder.Build();
+            //_token = Configuration["token"];
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private void Start()
+        {
+            var client = new DiscordClient();
+
+            client.OnConnected += (sender, e) => {
+                Console.WriteLine($"Connected as {client.User}!");
+            };
+        }
+    }
+}
