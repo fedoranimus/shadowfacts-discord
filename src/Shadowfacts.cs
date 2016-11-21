f488c30c38a4308492190237f0a8235562134d2f
Tim Turner <timdturner@gmail.com>
2016-11-21 17:15:17 -0500

    First Build

--- src/Shadowfacts.cs
+++ src/Shadowfacts.cs
@@ -6,25 +6,40 @@ namespace ShadowfactsDiscord
 {
     public class Shadowfacts
     {
-        private const string _token = "";
+        private static string _token = "";
         public static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
                             .AddJsonFile("config.json");
 
             var configuration = builder.Build();
-            //_token = Configuration["token"];
+            _token = configuration["token"];
 
             Console.WriteLine("Token: {0}", _token);
+
+            Start();
         }
 
-        private void Start()
+        private static void Start()
         {
             var client = new DiscordClient();
 
             client.OnConnected += (sender, e) => {
                 Console.WriteLine($"Connected as {client.User}!");
             };
+
+            client.OnMessageCreated += (sender, e) => {
+                DiscordMessage message = e.Message;
+                if(message.Author != client.User && message.Content.Contains("!ping"))
+                {
+                    message.Channel.SendMessage("pong!");
+                }
+            };
+
+            if(!client.Connect(_token).Result)
+            {
+                Console.WriteLine("Failed to connect!");
+            }
         }
     }
 }
